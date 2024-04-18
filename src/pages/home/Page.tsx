5916ea56fc7ade0f868031904b9faf5065c77252
ssumniee <suemink.dev@gmail.com>
2024-04-19 08:12:20 +0900

    feat: Video 섹션 구현 및 적용

--- src/pages/home/Page.tsx
+++ src/pages/home/Page.tsx
@@ -2,7 +2,8 @@ import classNames from "classnames/bind";
 
 import { HeaderComponent } from "../../components/header";
 import { MainSection } from "./sections/main";
-import { languageOptions } from "./constant";
+import { VideoSection } from "./sections/video";
+import { languageOptions, videoDataList } from "./constant";
 import styles from "./style.module.scss";
 
 const cx = classNames.bind(styles);
@@ -12,6 +13,14 @@ function HomePage() {
     <div className={cx("page")}>
       <HeaderComponent languageOptions={languageOptions} colorTheme="dark" />
       <MainSection />
+      {videoDataList.map((data, idx, arr) => (
+        <VideoSection
+          key={`section-${idx}`}
+          index={idx}
+          length={arr.length}
+          {...data}
+        />
+      ))}
     </div>
   );
 }
