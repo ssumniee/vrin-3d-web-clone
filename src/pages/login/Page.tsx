253b32fd41e5755465d7c47f7a45e574f4e7a4e3
ssumniee <suemink.dev@gmail.com>
2024-04-22 02:39:39 +0900

    feat: 랜딩 화면 이외 화면에 Header 컴포넌트 추가

--- src/pages/login/Page.tsx
+++ src/pages/login/Page.tsx
@@ -1,14 +1,18 @@
+import { useTranslation } from "react-i18next";
 import classNames from "classnames/bind";
 
+import { HeaderComponent } from "../../components/header";
 import styles from "./style.module.scss";
 
 const cx = classNames.bind(styles);
 
 function LoginPage() {
+  const { t } = useTranslation();
+
   return (
     <div className={cx("page")}>
-      <h1>Login</h1>
-      <p>로그인 페이지</p>
+      <HeaderComponent colorTheme="light" />
+      <h1>{t("login.title")}</h1>
     </div>
   );
 }
