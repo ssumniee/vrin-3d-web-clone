be8cbdf70d813d52be417559f8ca2042036f7961
ssumniee <suemink.dev@gmail.com>
2024-04-21 10:47:50 +0900

    feat: i18n 초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import "./locales/i18n";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
